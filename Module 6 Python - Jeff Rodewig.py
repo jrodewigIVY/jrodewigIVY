4cd7aca016c5e164262e8dfb3cbfe69090e4f453
jrodewigIVY <jrodewig2@ivytech.edu>
2024-11-29 12:57:20 -0500

    Add files via upload

---
+++ Module 6 Python - Jeff Rodewig.py
@@ -0,0 +1,4 @@
+c = "cats" #sets the letter c as cats
+d = "dogs" #sets the letter d as dogs
+s = ("it is raining " + c + " and " + d + ".") #sets the letter s as the sentence while adding variables c and d
+print(s)
